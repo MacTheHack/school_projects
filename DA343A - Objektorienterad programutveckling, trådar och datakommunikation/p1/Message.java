509edb7db297e545c16a7db2e5905f6e53d94f98
jonssonmattias <44271390+jonssonmattias@users.noreply.github.com>
2019-02-28 22:19:04 +0100

    Add files via upload

---
+++ DA343A - Objektorienterad programutveckling, trådar och datakommunikation/p1/Message.java
@@ -0,0 +1,26 @@
+package p1;
+
+import java.io.*;
+import javax.swing.*;
+
+public class Message implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Icon icon;
+	private String text;
+	
+	public Message(String text, Icon icon) {
+		this.text=text;
+		this.icon=icon;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+	public Icon getIcon() {
+		return icon;
+	}
+}
