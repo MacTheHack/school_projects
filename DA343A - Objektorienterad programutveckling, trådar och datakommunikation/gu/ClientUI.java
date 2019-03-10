2a007b454e6d174d03a1cbdb14b455f4f793e771
jonssonmattias <44271390+jonssonmattias@users.noreply.github.com>
2019-03-10 19:51:35 +0100

    Update ClientUI.java

--- DA343A - Objektorienterad programutveckling, trådar och datakommunikation/gu/ClientUI.java
+++ DA343A - Objektorienterad programutveckling, trådar och datakommunikation/gu/ClientUI.java
@@ -1,111 +1,161 @@
 package gu;
 
-import java.awt.EventQueue;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import javax.swing.JTextField;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.io.IOException;
+import java.awt.*;
 import java.awt.event.ActionEvent;
-import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.io.File;
 
-public class ClientUI {
+public class ClientUI extends JPanel implements ActionListener{
+	private final int port = 1500;
+	private final String ipAdress = "127.0.0.1";
 	private Client client;
-	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextArea textArea;
+	private boolean connected;
 	private User user;
+	private ImageIcon profileImage;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ClientUI window = new ClientUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private JPanel panelNorth = new JPanel();
+	private JPanel panelNorthCenter = new JPanel();
+	private JPanel panelNorthSouth = new JPanel();
+	private JPanel panelCenter = new JPanel();
+	private JPanel panelSouth = new JPanel();
+	private JPanel panelWhole = new JPanel();
 
-	/**
-	 * Create the application.
-	 */
-	public ClientUI(Client client) {
-		this.client=client;
-		
-		initialize();
-		frame.setVisible(true);
-	}
+	private JButton btnChoose = new JButton("Choose");
+	private JButton btnConnect = new JButton("Connect to server");
+	private JButton btnSend = new JButton("Send");
+	private JButton btnImage = new JButton("+");
+	private JButton btnDisconnect = new JButton("Disconnect");
+	private JButton btnShowList = new JButton("Show list");
+
+	private JTextPane image = new JTextPane();
+
+	private JTextField tf1 = new JTextField();
+	private JTextPane textPaneViewer = new JTextPane();
+	private JTextPane textPaneMessage = new JTextPane();
+
+	private JLabel lblName = new JLabel("Username: ", SwingConstants.RIGHT);
 
 	public ClientUI() {
-		initialize();
-	}
+		actionListener();
+		setLayout();
+		setPreferredSize();
+		panelWhole.add(panelNorth);
+		panelWhole.add(panelCenter);
+		panelWhole.add(panelSouth);
+		add(panelNorth, BorderLayout.NORTH);
+		add(panelCenter, BorderLayout.CENTER);
+		add(panelSouth, BorderLayout.SOUTH);
+		panelNorthCenter.add(btnChoose);
+		panelNorthCenter.add(image);
+		panelNorthCenter.add(lblName);
+		panelNorthCenter.add(tf1);
+		panelNorthSouth.add(btnConnect);
+		panelNorthSouth.add(btnDisconnect);
+		panelNorthSouth.add(btnShowList);
+		panelNorth.add(panelNorthCenter, BorderLayout.CENTER);
+		panelNorth.add(panelNorthSouth, BorderLayout.SOUTH);
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame("Client");
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.NORTH);
-		GridLayout gbl_panel = new GridLayout(2,2);
-		panel.setLayout(gbl_panel);
-		
-		textField = new JTextField();
-		panel.add(textField);
-		textField.setColumns(10);
-		
-		JButton btnNewButton = new JButton("Connect");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				client.connect();
-				user = new User(textField.getText(),new ImageIcon("Images/gubbe.jpg"));
-				client.send(user);
-				textField.setEnabled(false);
-				btnNewButton.setEnabled(false);
+		textPaneViewer.setEditable(false);
+		textPaneViewer.setBackground(new Color(220,220,220));
+		panelCenter.add(textPaneViewer);
+
+		panelSouth.add(btnSend, BorderLayout.EAST);
+		panelSouth.add(textPaneMessage, BorderLayout.CENTER);
+		panelSouth.add(btnImage,BorderLayout.WEST);
+	}
+	public void actionListener() {
+		btnChoose.addActionListener(this);
+		btnConnect.addActionListener(this);
+		btnSend.addActionListener(this);
+		btnImage.addActionListener(this);
+	}
+	public void setLayout() {
+		setLayout(new BorderLayout());
+		panelNorthCenter.setLayout(new GridLayout(1,4));
+		panelNorthSouth.setLayout(new GridLayout(3,1));
+		panelNorth.setLayout(new BorderLayout());
+		panelCenter.setLayout(new BorderLayout());
+		panelSouth.setLayout(new BorderLayout());
+	}
+	public void setPreferredSize() {
+		btnChoose.setPreferredSize(new Dimension(80,30));
+		btnChoose.setOpaque(true);
+		btnConnect.setPreferredSize(new Dimension(80,30));
+		btnConnect.setOpaque(true);
+		btnSend.setPreferredSize(new Dimension(80,30));
+		btnSend.setOpaque(true);
+		btnImage.setPreferredSize(new Dimension(80,30));
+		btnImage.setOpaque(true);
+		tf1.setPreferredSize(new Dimension(150, 40));
+	}
+	public void append(Object obj) {
+		if(!textPaneViewer.getText().isEmpty()) {
+			textPaneViewer.setText(textPaneViewer.getText()+"\n"+obj.toString());
+		}
+		else 
+			textPaneViewer.setText(obj.toString());
+	}
+	public void connectionFailed() {
+		System.out.println("connection failed");
+	}
+	public ImageIcon resizeImage(String ImagePath, JTextPane tp){
+		ImageIcon MyImage = new ImageIcon(ImagePath);
+		Image img = MyImage.getImage();
+		Image newImg = img.getScaledInstance(tp.getWidth(), tp.getHeight(), Image.SCALE_SMOOTH);
+		ImageIcon image = new ImageIcon(newImg);
+		return image;
+	}
+	private ImageIcon imageChooser() {
+		ImageIcon img = null;
+		JFileChooser file = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("*.Images", "jpg","gif","png");
+		file.addChoosableFileFilter(filter);
+		int result = file.showSaveDialog(null);
+		if(result == JFileChooser.APPROVE_OPTION){
+			File selectedFile = file.getSelectedFile();
+			String path = selectedFile.getAbsolutePath();
+			img = resizeImage(path, image);
+		}
+		else if(result == JFileChooser.CANCEL_OPTION){
+			System.out.println("No File Select");
+		}
+		return img;
+	}
+	public void actionPerformed(ActionEvent e) {
+
+		if(btnChoose == e.getSource()) {
+			profileImage=imageChooser();
+			image.insertIcon(profileImage);
+		}
+		if(btnConnect == e.getSource()) {
+			String username = tf1.getText().trim();
+			user = new User(username,profileImage);
+			client = new Client(ipAdress, port, user, this);
+			if(!client.start()) return;
+			tf1.setEnabled(false);
+			connected = true;
+		}
+		if(btnSend == e.getSource()) {
+			if(connected) {
+				client.sendMessage(new Message(Message.MESSAGE, textPaneMessage.getText(),null,user,null));
 			}
-		});
-		panel.add(btnNewButton);
-		
-		textField_1 = new JTextField();
-		panel.add(textField_1);
-		textField_1.setColumns(10);
-		
-		JButton btnNewButton_1 = new JButton("Send");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				Message msg = new Message(textField_1.getText(),null,user,null);
-				client.send(msg);
-				textField_1.setText("");
-			}
-		});
-		panel.add(btnNewButton_1);
-		
-		textArea = new JTextArea();
-		frame.getContentPane().add(textArea, BorderLayout.CENTER);
-	}
+		}
+		if(btnImage == e.getSource()) {
 
-	public void setResult(String result) {
-		textArea.append(result+"\n");
-	}
-
-	private class CallbackClass implements CallbackInterface {
-		public void update(Object result) {
-			setResult((String)result);
 		}
 	}
+
+	public static void main(String[] args) {
+		ClientUI chatUI = new ClientUI();
+		JFrame frame = new JFrame("Chat");
+		frame.setPreferredSize(new Dimension(450, 600));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.pack();
+		frame.add(chatUI);
+	}
 }
+
